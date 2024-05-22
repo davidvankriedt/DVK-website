1969c01e6ab36eb6074c6826f83ea8219106e134
davidvankriedt <davidvankriedt@icloud.com>
2024-05-22 20:12:28 +1000

    Slight tweaks + prototype of rotating 3d cube

--- public/script.js
+++ public/script.js
@@ -1,5 +1,9 @@
 
 const color = ['white', '#f2ddae', '#e2f2ae', '#c8f2ae', '#aef2ed', '#b5aef2'];
+const cube = document.querySelector(".cube");
+let mouseX = 0;
+let mouseY = 0;
+const rotationValue = 270
 
 body = document.body;
 titleBtn = document.getElementById("titleBtn");
@@ -10,3 +14,13 @@ function changeColour() {
   body.style.background = color[randomNum];
   titleBtn.style.background = color[randomNum];
 }
+
+const handleMouseMove = (event) => {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+  rotateX = -(mouseY / window.innerHeight - 0.5) * rotationValue;
+  rotateY = (mouseX / window.innerWidth - 0.5) * rotationValue;
+  cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+}
+
+window.addEventListener("mousemove", handleMouseMove);
